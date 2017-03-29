10c676b7632ec33a9172e66f0ec14bf5dc39ea5f
Mikhail Morfikov <mmorfikov@gmail.com>
2017-03-29 15:16:17 +0200

    Remove selinux=permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,7 @@ BOARD_KERNEL_TAGS_OFFSET  := 0x0df88000
 BOARD_KERNEL_SEPARATED_DT := false
 BOARD_KERNEL_CMDLINE := \
 	bootopt=64S3,32N2,64N2
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := \
 	--base $(BOARD_KERNEL_BASE) \
 	--pagesize $(BOARD_KERNEL_PAGESIZE) \
